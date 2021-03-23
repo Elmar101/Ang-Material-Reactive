a8412f0dc3ae7825cb0df109f2db18cabe96167d
Elmar Amanov <Elmar.amanov10@gmail.com>
2021-03-24 00:49:52 +0400

    Reactiv Angular

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,65 @@
+import {Component, OnInit} from '@angular/core';
+import {Course, sortCoursesBySeqNo} from '../model/course';
+import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
+import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import { CoursesService } from '../services/courses.service';
+import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/message.service';
+
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  beginnerCourses$:Observable<Course[]>;
+
+  advancedCourses$: Observable<Course[]>;
+
+
+  constructor(
+    private coursesService:CoursesService,
+    private loadingService:LoadingService,
+    private messagesService:MessagesService) {
+  }
+
+  ngOnInit() {
+    this.reloadCourses();
+
+  }
+
+  reloadCourses(){
+
+    const courses$ = this.coursesService.loadAllCourses()
+    .pipe(
+      map(courses=>courses.sort(sortCoursesBySeqNo)),
+      catchError(err=>{
+        const message = "Could not load Courses";
+        this.messagesService.showErrors(message);
+        console.log(message,err);
+        return throwError(err);
+
+      })
+    );
+
+    const loadCourses$ = this.loadingService.showLoderUntilCompleted(courses$);
+
+    this.beginnerCourses$ = loadCourses$.pipe(
+      map(courses => courses.filter(course => course.category ==='BEGINNER') )
+    )
+
+    this.advancedCourses$ = loadCourses$.pipe(
+      map(courses => courses.filter(course => course.category ==='ADVANCED') )
+    )
+  }
+
+}
+
+
+
+
---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {Message} from '../model/message';
+import {tap} from 'rxjs/operators';
+import { MessagesService } from './message.service';
+
+@Component({
+  selector: 'messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+  showMessage = true;
+
+  errors$: Observable<string[]>;
+
+  constructor(public messagesService: MessagesService) {
+    console.log("Created messages component");
+
+  }
+
+  ngOnInit() {
+    this.errors$ = this.messagesService.errors$
+    .pipe(
+      tap( ()=> this.showMessage = true)
+    )
+  }
+
+
+  onClose() {
+    this.showMessage = false;
+
+  }
+
+}
